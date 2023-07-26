3852f5c3261c310f9b7a45b6afa93e56aba6eca1
Fabian Schneider <139592458+fabianswebworld@users.noreply.github.com>
2023-07-26 10:22:03 +0200

    extended config file for new parameters in v1.3.0

--- includes/ttxweb_config.php
+++ includes/ttxweb_config.php
@@ -4,12 +4,15 @@
 
 // configuration
 
-const TTXWEB_TEMPLATE    = 'default';                         // folder to use for HTML templates, default: 'default'
+const TTXWEB_TEMPLATE    = 'hr';                              // folder to use for HTML templates, default: 'default'
+const TTXWEB_REFRESH     = 30;                                // seconds for automatic refresh via XHR, default: 0 = disabled
 const EP1_PATH           = 'ep1/';                            // relative path to EP1 files
 const EP1_LANGUAGE       = 'de-DE';                           // teletext language (currently only 'de-DE'; 'en-GB')
 const EP1_DECODE_X26     = true;                              // decode packet X/26 (level 1.5 characters)
 const EP1_ALWAYS_REVEAL  = false;                             // always reveal concealed text on load
 const NO_PAGE_STRING     = 'Seite nicht vorhanden';           // string for 'Page not found'
+const TO_PAGE_STRING     = 'Zu Seite';                        // string for 'Jump to page...'
+const TO_SUBPAGE_STRING  = 'Zu Unterseite';                   // string for 'Jump to subpage...'
 
 // configuration of custom header line (row 0) moved to template_config.php in template folder!
 
