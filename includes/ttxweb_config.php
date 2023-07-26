7f06fc2f29286f4ce1a0e26e323428c4b96ef235
Fabian Schneider <139592458+fabianswebworld@users.noreply.github.com>
2023-07-26 15:12:27 +0200

    update config file for 1.3.0 features, change default template

--- includes/ttxweb_config.php
+++ includes/ttxweb_config.php
@@ -4,7 +4,7 @@
 
 // configuration
 
-const TTXWEB_TEMPLATE    = 'hr';                              // folder to use for HTML templates, default: 'default'
+const TTXWEB_TEMPLATE    = 'default';                         // folder to use for HTML templates, default: 'default'
 const TTXWEB_REFRESH     = 30;                                // seconds for automatic refresh via XHR, default: 0 = disabled
 const EP1_PATH           = 'ep1/';                            // relative path to EP1 files
 const EP1_LANGUAGE       = 'de-DE';                           // teletext language (currently only 'de-DE'; 'en-GB')
